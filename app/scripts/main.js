0129e9a7a105d494faebe272f6adff0f1a7e7aca
inabajunmr <inabajun.for.regi@gmail.com>
2018-07-28 17:47:04 +0900

    executable

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,6 +12,7 @@ gh_diff.main = function() {
     let targets = gh_diff.get_targets();
 
     Array.prototype.forEach.call(targets, target => {
+
         let title = target.title;
         let groups = title.match("^(.*) → (.*)$");
         if(groups == null){
@@ -22,15 +23,23 @@ gh_diff.main = function() {
         // get before and after file path
         let before = groups[1];
         let after = groups[2];
-        
-        // get diff
-        // change visual
+        let diff = JsDiff.diffChars(after, before);
 
+        let diffNode = document.createDocumentFragment();
+        diff.forEach(function(part){
+            let node = document.createElement('span');
+            node.appendChild(document.createTextNode(part.value));    
+            if(part.added){
+                node.style.cssText = "background-color: #acf2bd;"; 
+            }else if(part.removed){
+                node.style.cssText = "background-color: #fdb8c0;"; 
+            }
+            diffNode.appendChild(node);
+          });
+
+          target.removeChild(target.firstChild);
+          target.appendChild(diffNode);
     });
-
-
-
-
 }
 
 gh_diff.main()
