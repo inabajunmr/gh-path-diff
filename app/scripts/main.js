d0d612229922420fcabd5d9b220167841d48d5ce
inabajunmr <inabajun.for.regi@gmail.com>
2018-07-28 17:16:17 +0900

    wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-alert("test");
+`use strict`;
+
+gh_diff = {};
+
+gh_diff.get_targets = function(){
+    return document.querySelectorAll(".file-info > a");
+}
+
+gh_diff.main = function() {
+    
+    // correct filepath diff elements(a tag)
+    let targets = gh_diff.get_targets();
+
+    Array.prototype.forEach.call(targets, target => {
+        let title = target.title;
+        let groups = title.match("^(.*) → (.*)$");
+        if(groups == null){
+            // no diff for file path
+            return;
+        }
+
+        // get before and after file path
+        let before = groups[1];
+        let after = groups[2];
+        
+        // get diff
+        // change visual
+
+    });
+
+
+
+
+}
+
+gh_diff.main()
